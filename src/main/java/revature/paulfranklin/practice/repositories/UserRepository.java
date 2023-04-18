8f0e6c8119b281494a47835325a7dbb579bc48d1
Paul Franklin <paul219@revature.net>
2023-04-17 23:14:52 -0400

    Create UserRepository.java

---
+++ src/main/java/revature/paulfranklin/practice/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package revature.paulfranklin.practice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import revature.paulfranklin.practice.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
