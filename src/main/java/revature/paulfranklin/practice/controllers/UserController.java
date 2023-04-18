9349e0dfe2b709a7adaed9cf1767fb4199c46e4a
Paul Franklin <paul219@revature.net>
2023-04-18 13:15:58 -0400

    Update UserController.java

--- src/main/java/revature/paulfranklin/practice/controllers/UserController.java
+++ src/main/java/revature/paulfranklin/practice/controllers/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import revature.paulfranklin.practice.dtos.requests.NewUserRequest;
 import revature.paulfranklin.practice.dtos.responses.Principal;
+import revature.paulfranklin.practice.entities.User;
+import revature.paulfranklin.practice.services.TokenService;
 import revature.paulfranklin.practice.services.UserService;
 
 @CrossOrigin
@@ -11,22 +13,30 @@ import revature.paulfranklin.practice.services.UserService;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+    private final TokenService tokenService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, TokenService tokenService) {
         this.userService = userService;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
-    public void signup(@RequestBody NewUserRequest req) {
+    public Principal signup(@RequestBody NewUserRequest req) {
         if (req.getUsername() == null || req.getPassword() == null) {
             throw new RuntimeException("Missing username or password");
         }
 
+        User user = null;
         try {
-            userService.createNewUser(req);
+            user = userService.createNewUser(req);
         } catch (Exception exception) {
             throw new RuntimeException(exception.getMessage());
         }
+
+        Principal principal = new Principal(user.getUserId(), user.getUsername(), user.getActive(), user.getUserRole().getRole());
+        tokenService.createNewToken(principal);
+
+        return principal;
     }
 }
