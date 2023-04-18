14528131f374f733358dc57369d3ceddfdcd54b6
Paul Franklin <paul219@revature.net>
2023-04-18 17:40:14 -0400

    Update FriendshipRepository.java

--- src/main/java/revature/paulfranklin/practice/repositories/FriendshipRepository.java
+++ src/main/java/revature/paulfranklin/practice/repositories/FriendshipRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import revature.paulfranklin.practice.entities.Friendship;
 
+import java.util.List;
+
 @Repository
 public interface FriendshipRepository extends JpaRepository<Friendship, String> {
+    List<Friendship> findAllByUserUserId(String userId);
 }
