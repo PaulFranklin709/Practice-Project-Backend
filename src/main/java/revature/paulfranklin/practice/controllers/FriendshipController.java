f6ab09c57b43c5e090d1fca79ca901898b2a72e7
Paul Franklin <paul219@revature.net>
2023-04-18 17:42:50 -0400

    Create FriendshipController.java

---
+++ src/main/java/revature/paulfranklin/practice/controllers/FriendshipController.java
@@ -0,0 +1,34 @@
+package revature.paulfranklin.practice.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import revature.paulfranklin.practice.dtos.responses.Principal;
+import revature.paulfranklin.practice.entities.Friendship;
+import revature.paulfranklin.practice.services.FriendshipService;
+import revature.paulfranklin.practice.services.TokenService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/friendships")
+public class FriendshipController {
+    private final FriendshipService friendshipService;
+    private final TokenService tokenService;
+
+    public FriendshipController(FriendshipService friendshipService, TokenService tokenService) {
+        this.friendshipService = friendshipService;
+        this.tokenService = tokenService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public List<Friendship> getFriends(HttpServletRequest servReq) {
+        String token = servReq.getHeader("authorization");
+
+        Principal principal = tokenService.retrievePrincipalFromToken(token);
+
+        return friendshipService.getFriendsByUserId(principal.getUserId());
+    }
+}
