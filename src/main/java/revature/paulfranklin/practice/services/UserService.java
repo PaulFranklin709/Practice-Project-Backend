6336d5b0c448cb650b542cb91a6a34b75108a28d
Paul Franklin <paul219@revature.net>
2023-04-18 16:52:50 -0400

    Update UserService.java

--- src/main/java/revature/paulfranklin/practice/services/UserService.java
+++ src/main/java/revature/paulfranklin/practice/services/UserService.java
@@ -1,6 +1,7 @@
 package revature.paulfranklin.practice.services;
 
 import org.springframework.stereotype.Service;
+import revature.paulfranklin.practice.dtos.requests.NewLoginRequest;
 import revature.paulfranklin.practice.dtos.requests.NewUserRequest;
 import revature.paulfranklin.practice.entities.User;
 import revature.paulfranklin.practice.entities.UserRole;
@@ -20,6 +21,11 @@ public class UserService {
     public User createNewUser(NewUserRequest req) {
         User user = new User(UUID.randomUUID().toString(), req.getUsername(), req.getPassword(), true, new UserRole(Role.USER));
         userRepository.save(user);
+        return user;
+    }
+
+    public User getUser(NewLoginRequest req) {
+        User user = userRepository.findByUsername(req.getUsername());
         return user;
     }
 }
