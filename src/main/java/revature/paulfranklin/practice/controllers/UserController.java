fbea768c28591e15f1fc276f7b996d5ee8922dfd
Paul Franklin <paul219@revature.net>
2023-04-19 15:52:23 -0400

    Update UserController.java

--- src/main/java/revature/paulfranklin/practice/controllers/UserController.java
+++ src/main/java/revature/paulfranklin/practice/controllers/UserController.java
@@ -1,5 +1,7 @@
 package revature.paulfranklin.practice.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +22,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
     private final TokenService tokenService;
 
