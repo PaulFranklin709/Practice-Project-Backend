0fe6489345de91ab882197b40bf61f2e8308c80b
Paul Franklin <paul219@revature.net>
2023-04-18 21:32:53 -0400

    Update FriendshipController.java

--- src/main/java/revature/paulfranklin/practice/controllers/FriendshipController.java
+++ src/main/java/revature/paulfranklin/practice/controllers/FriendshipController.java
@@ -2,10 +2,12 @@ package revature.paulfranklin.practice.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import revature.paulfranklin.practice.dtos.requests.NewFriendRequest;
 import revature.paulfranklin.practice.dtos.responses.Principal;
-import revature.paulfranklin.practice.entities.Friendship;
+import revature.paulfranklin.practice.entities.User;
 import revature.paulfranklin.practice.services.FriendshipService;
 import revature.paulfranklin.practice.services.TokenService;
+import revature.paulfranklin.practice.services.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -16,10 +18,12 @@ import java.util.List;
 public class FriendshipController {
     private final FriendshipService friendshipService;
     private final TokenService tokenService;
+    private final UserService userService;
 
-    public FriendshipController(FriendshipService friendshipService, TokenService tokenService) {
+    public FriendshipController(FriendshipService friendshipService, TokenService tokenService, UserService userService) {
         this.friendshipService = friendshipService;
         this.tokenService = tokenService;
+        this.userService = userService;
     }
 
     @GetMapping
@@ -30,5 +34,25 @@ public class FriendshipController {
         Principal principal = tokenService.retrievePrincipalFromToken(token);
 
         return friendshipService.getFriendsByUserId(principal.getUserId());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void newFriend(@RequestBody NewFriendRequest req, HttpServletRequest servReq) {
+        String token = servReq.getHeader("authorization");
+        if (req.getFriendName() == null) {
+            throw new RuntimeException("Missing friend name");
+        }
+
+        Principal principal = tokenService.retrievePrincipalFromToken(token);
+
+        User user = userService.getUserByUsername(principal.getUsername());
+        User friend = userService.getUserByUsername(req.getFriendName());
+
+        try {
+            friendshipService.createNewFriendship(user, friend);
+        } catch (Exception exception) {
+            throw new RuntimeException(exception.getMessage());
+        }
     }
 }
