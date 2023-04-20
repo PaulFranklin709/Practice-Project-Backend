bbb5c63b1b1793bfc8fbd8067aa306d0bfb81cea
Paul Franklin <paul219@revature.net>
2023-04-19 20:26:56 -0400

    Update ReimbursementRepository.java

--- src/main/java/revature/paulfranklin/practice/repositories/ReimbursementRepository.java
+++ src/main/java/revature/paulfranklin/practice/repositories/ReimbursementRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import revature.paulfranklin.practice.entities.Reimbursement;
 
+import java.util.List;
+
 @Repository
 public interface ReimbursementRepository extends JpaRepository<Reimbursement, String> {
+    List<Reimbursement> findAllByAuthorUserId(String authorId);
 }
